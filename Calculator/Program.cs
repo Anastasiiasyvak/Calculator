4e2e5823b8476bb26168ff72ce9b3ca466b2e4ad
Anastasiiasyvak <asyvak@kse.org.ua>
2023-02-22 21:10:41 +0200

    add ^ operation

--- Calculator/Program.cs
+++ Calculator/Program.cs
@@ -21,7 +21,7 @@ foreach (var x in input)
             index ++ ; 
         }
     }
-    else if (x == '+' || x == '-' ||  x == '*' || x == '/')
+    else if (x == '+' || x == '-' ||  x == '*' || x == '/' || x == '^')
     {
         if (b != "")
         {
@@ -45,11 +45,11 @@ int priority = 0; // початковий пріорітет для першог
 
 foreach (string x in result)
 {
-    if (!(x == "+" || x == "-" || x == "*" || x == "/"))
+    if (!(x == "+" || x == "-" || x == "*" || x == "/" || x == "^"))
     {
         queue.Enqueue(x);
     }
-    else if (x == "+" || x == "-" || x == "*" || x == "/")
+    else if (x == "+" || x == "-" || x == "*" || x == "/" || x == "^")
     {
         
         Dictionary<string, int> operatorPriorities = new Dictionary<string, int>
@@ -57,11 +57,12 @@ foreach (string x in result)
             { "+", 0 },
             { "-", 0 },
             { "*", 1 },
-            { "/", 1 }
+            { "/", 1 },
+            { "^", 2 }
         };
 
         int currentPriority = operatorPriorities[x];
-        while (stack.Count > 0 && (stack.Peek().ToString() == "*" || stack.Peek().ToString() == "/")) // peek перевіряє останній оператор 
+        while (stack.Count > 0 && (stack.Peek().ToString() == "*" || stack.Peek().ToString() == "/" || stack.Peek().ToString() == "^")) // peek перевіряє останній оператор 
         {
             queue.Enqueue(stack.Pop().ToString());
         }
@@ -74,6 +75,14 @@ foreach (string x in result)
             if (lastOperator == "+" || lastOperator == "-")
             {
                 lastPriority = 0;
+            }
+            else if (lastOperator == "*" || lastOperator == "/")
+            {
+                lastPriority = 1;
+            }
+            else if (lastOperator == "^")
+            {
+                lastPriority = 2;
             }
 
             if (currentPriority <= lastPriority)
@@ -99,18 +108,18 @@ foreach (var item in queue)
 }
 
 // =============================================================
-
-Stack stack2 = new Stack();
-foreach (var item in queue)
-{
-    if (!(item == "+" || item == "-" || item == "*" || item == "/"))
-    {
-        stack2.Push(item);
-        int firstresult =  Convert.ToInt32(item);
-    }
-    else if (item == "+" || item == "-" || item == "*" || item == "/")
-    {
-        stack2.Pop();
-        
-    }
-}
+//
+// Stack stack2 = new Stack();
+// foreach (var item in queue)
+// {
+//     if (!(item == "+" || item == "-" || item == "*" || item == "/"))
+//     {
+//         stack2.Push(item);
+//         int firstresult =  Convert.ToInt32(item);
+//     }
+//     else if (item == "+" || item == "-" || item == "*" || item == "/")
+//     {
+//         stack2.Pop();
+//         
+//     }
+// }
