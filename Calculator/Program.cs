71688b0251eeba3f9e638d3a68f83a7ec6417159
Anastasiiasyvak <asyvak@kse.org.ua>
2023-02-22 10:12:31 +0200

    add Polish notation

--- Calculator/Program.cs
+++ Calculator/Program.cs
@@ -1,43 +1,99 @@
-﻿string input = Console.ReadLine();  // зчитує input
+﻿using System.Collections; // дає можливість працювати з масивами, списками та чергами 
+
+string input = Console.ReadLine();  
 string b = ""; // створюємо порожній буфер куди будуть вноситись тєкущі токени 
 string[] result = new String[50]; // створюємо list з максимум 50 символами куди вносяться усі токени
-int index = 0; // починаємо з індекса нуль
+int index = 0; 
 // char 'd' одиничний символ, нам буде повертатися після foreach
 // "d" string 
-foreach (var x in input)   // для кожного x елемента у input який ввів користувач 
+foreach (var x in input)    
 {
-    if (Char.IsDigit(x)) // якщо char елемент це число 
+    if (Char.IsDigit(x))  
     {
-        b += x; // додаємо x до буферу 
+        b += x; 
     }
     else if (Char.IsWhiteSpace(x)) // якщо x це пробіл
     {
-        if (b != "") // якщо b не пустий, то він додається до result
+        if (b != "") 
         {
-            result[index] = b; // додаємо в список result з індексом який спочатку є нулем b 
-            b = ""; // опустошаємо наш буфер 
-            index += 1; // в наступний раз у нас індекс збільшиться на 1
+            result[index] = b; 
+            b = ""; 
+            index ++ ; 
         }
     }
-    else if (x == '(' || x == ')' || x == '+' || x == '-' || x == '*' || x == '/')
+    else if (x == '+' || x == '-' ||  x == '*' || x == '/')
     {
         if (b != "")
         {
-            result[index] = b; // додаємо в список result з індексом який спочатку є нулем b 
+            result[index] = b; 
             b = "";
-            index += 1;
+            index ++ ;
         }
-        result[index] = x.ToString(); // переводимо x з char на string 
-        index += 1;
+        result[index] = x.ToString(); 
+        index ++ ;
     }
 }
 
-if (b != "")  // додає останній токен якщо він не є порожнім
+if (b != "")  
 {
     result[index] = b;
 }
 
+Queue queue = new Queue();
+Stack stack = new Stack();
+int priority = 0; // початковий пріорітет для першого оператора
+
 foreach (string x in result)
 {
-    Console.WriteLine(x);
+    if (!(x == "+" || x == "-" || x == "*" || x == "/"))
+    {
+        queue.Enqueue(x);
+    }
+    else if (x == "+" || x == "-" || x == "*" || x == "/")
+    {
+        
+        Dictionary<string, int> operatorPriorities = new Dictionary<string, int>
+        {
+            { "+", 0 },
+            { "-", 0 },
+            { "*", 1 },
+            { "/", 1 }
+        };
+
+        int currentPriority = operatorPriorities[x];
+        while (stack.Count > 0 && (stack.Peek().ToString() == "*" || stack.Peek().ToString() == "/")) // peek перевіряє останній оператор 
+        {
+            queue.Enqueue(stack.Pop().ToString());
+        }
+
+        if (stack.Count > 0 && (stack.Peek().ToString() == "+" || stack.Peek().ToString() == "-"))
+        {
+            int lastPriority = 0;
+            string lastOperator = stack.Peek().ToString();
+
+            if (lastOperator == "+" || lastOperator == "-")
+            {
+                lastPriority = 0;
+            }
+
+            if (currentPriority <= lastPriority)
+            {
+                lastOperator = stack.Pop().ToString();
+                queue.Enqueue(lastOperator);
+            }
+        }
+
+        stack.Push(x);
+    }
+}
+
+while (stack.Count > 0)
+{
+    string lastOperator = stack.Pop().ToString();
+    queue.Enqueue(lastOperator);
+}
+
+foreach (var item in queue)
+{
+    Console.Write(item + "");
 }
